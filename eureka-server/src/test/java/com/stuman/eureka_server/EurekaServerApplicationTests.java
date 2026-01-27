916d079598e708971c52ff21a0b0cbbbb7014ec8
Trinh Xuan Huan <trinhxuanhuan1405@gmail.com>
2026-01-27 21:21:57 +0700

    Initial setup: student management microservices

---
+++ eureka-server/src/test/java/com/stuman/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuman.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
