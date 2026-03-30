e08c79a746351dd49d5029c6df509b17f1c83096
Trinh Xuan Huan <trinhxuanhuan1405@gmail.com>
2026-03-31 00:00:18 +0700

    feat: add StudentDeletedEvent and StudentUpdatedEvent

---
+++ common-event/src/main/java/com/stuman/common_event/StudentDeletedEvent.java
@@ -0,0 +1,15 @@
+package com.stuman.common_event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDeletedEvent {
+
+    private Long studentId;
+}
